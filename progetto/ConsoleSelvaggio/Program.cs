14e7dd5778b52c26dab5fec2bf580698cb4ffe01
rinaka1504 <rnafsir@gmail.com>
2025-06-26 15:26:11 +0200

    create

---
+++ progetto/ConsoleSelvaggio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleSelvaggio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
